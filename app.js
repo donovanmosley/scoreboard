d90ccef6a7b394eb1faeeac061034c789c9b8555
Donovan Mosley <dmosley28@gmail.com>
2020-08-13 06:18:48 -0400

    Added event handler for updating the Counter state using React's build-in state method, updated a this.incrementScore OnClick event that calls the event handler when clicked

--- app.js
+++ app.js
@@ -43,17 +43,20 @@ const Player = (props) => {
 }
 
 class Counter extends React.Component {
- 
   state = {
     score: 0
   };
+
+  incrementScore() {
+    console.log('Hi, from inside incrementScore!');
+  }
 
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
         <span className="counter-score">{ this.state.score }</span>
-        <button className="counter-action increment"> + </button>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
